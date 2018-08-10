c7cd0bb0bbc479c5bc2324f25c697cda7994d6e3
Vladimir Kuznetsov <vladimir.kazansky@gmail.com>
2018-08-10 23:09:40 +0200

    Unify "Makefile"s and .gitignore

--- src/examples/01_hello_world/hello.asm
+++ src/examples/01_hello_world/hello.asm
@@ -4,23 +4,30 @@
 ; ./hello
 ; echo $? # for return code
 ; ld hello.o -e start -o hello
+%ifidn __OUTPUT_FORMAT__, elf64
+    %define     SYS_WRITE       0x0000001
+    %define     SYS_EXIT        0x000003C
+%elifidn __OUTPUT_FORMAT__, macho64
+    %define     SYS_WRITE       0x2000004
+    %define     SYS_EXIT        0x2000001
+%endif
 
-        global      start                       ; ld [-e main] # for entrypoint
+global  start                           ; ld [-e main] # for entrypoint
 
-        section     .data
-msg:    db          "Hello, world!!!", 0x0a     ; 0x0a = 10 = /n
-.len:   equ         $ - msg
+section .data
+msg:    db      "Hello, world!!!", 0x0a ; 0x0a = 10 = /n
+.len:   equ     $ - msg
 
-        section     .text
+section     .text
 start:
-        mov         rax, 0x2000004          ; system call number should be stored in rax
-        mov         rdi, 1                  ; argument #1 in rdi: where to write, stdout = 1
-        mov         rsi, msg                ; argument #2 where does the string starg
-        mov         rdx, msg.len            ; argument #3 how many bytes to write
-        syscall
+    mov         rax, SYS_WRITE          ; system call number should be stored in rax
+    mov         rdi, 1                  ; argument #1 in rdi: where to write, stdout = 1
+    mov         rsi, msg                ; argument #2 where does the string starg
+    mov         rdx, msg.len            ; argument #3 how many bytes to write
+    syscall
 
-        ; exit
-        mov         rax, 0x2000001          ; exit syscall number
-        xor         rdi, rdi                ; exit status 0
-        ; mov rdi, 42
-        syscall
+    ; exit
+    mov         rax, SYS_EXIT           ; exit syscall number
+    xor         rdi, rdi                ; exit status 0
+    ; mov rdi, 42
+    syscall
